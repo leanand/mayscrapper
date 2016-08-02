b3bb48d5dd7e5e23f933cd4208d8cc60e9afb308
Lovelin Anand <lovelinanand@hacedor91.local>
2016-08-02 10:17:47 -0400

    Tweaks

--- msgWorker.sh
+++ msgWorker.sh
@@ -1,4 +1,4 @@
-lastPage="$(redis-cli get LAST_PAGE)"
-emailList="$(redis-cli --raw hlen REQUIRED_EMAILS)"
+lastPage="$(/usr/local/bin/redis-cli get LAST_PAGE)"
+emailList="$(/usr/local/bin/redis-cli --raw hlen REQUIRED_EMAILS)"
 
 curl http://textbelt.com/text -d number=5053927444 -d "message=Last Page : ${lastPage}; Total Email :${emailList}"
